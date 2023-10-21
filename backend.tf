79675e69a6556da3ac1d44961e8788cae4ba7349
sudhamsh <sudhamsh1996@gmail.com>
2023-10-21 19:13:14 +0530

    added the proper region in backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "theinfinitebucketinthisaccount"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-lock"
   }
 }
